76529a5894b7cf2ca01aa5cdcac2a21c7b489b35
Karen Dayana Suarez Ramos <karensuarez1120@gmail.com>
2024-02-29 12:02:41 -0500

    Modificacion en los nombres de las variables, e implementacion de lista para el car's color

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const IP_ADDRESS = '10.4.74.206'
+const IP_ADDRESS = '10.4.61.86'
 function showRegistrationForm() {
   document.getElementById("registration-form").style.display = "block";
   document.getElementById("cars-list").style.display = "none";
